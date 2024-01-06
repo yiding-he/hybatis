ff469b0277967e3ffa56ecfcc308dd60d4ad9d3e
YidingHe <yiding.he@gmail.com>
2024-01-07 00:44:34 +0800

    改进 Conditions 的反序列化能力，能够从简化版本的 JSON 构建

--- src/test/java/com/hyd/hybatis/ConditionsTest.java
+++ src/test/java/com/hyd/hybatis/ConditionsTest.java
@@ -1,8 +1,10 @@
 package com.hyd.hybatis;
 
 import com.alibaba.fastjson2.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConditionsTest {
@@ -27,7 +29,29 @@ class ConditionsTest {
     public void testFastjson2Serialization() {
         var conditions = new Conditions().withColumn("a").nin(1, 2, 3);
         var json = JSON.toJSONString(conditions);
+        System.out.println("json = " + json);
         var deserialized = JSON.parseObject(json, Conditions.class);
         assertEquals(conditions, deserialized);
+
+        // 简化版本
+        var json2 = "{\"query\":{\"a\":{\"nin\":[1,2,3]}}}";
+        var deserialized2 = JSON.parseObject(json2, Conditions.class);
+        assertEquals(conditions, deserialized2);
+    }
+
+    @Test
+    public void testJacksonSerialization() throws Exception {
+        var objectMapper = new ObjectMapper();
+        var conditions = new Conditions().withColumn("a").nin(1, 2, 3);
+        var json = objectMapper.setSerializationInclusion(NON_NULL).writeValueAsString(conditions);
+        System.out.println("json = " + json);
+
+        var deserialized = objectMapper.readValue(json, Conditions.class);
+        assertEquals(conditions, deserialized);
+
+        // 简化版本
+        var json2 = "{\"query\":{\"a\":{\"nin\":[1,2,3]}}}";
+        var deserialized2 = objectMapper.readValue(json2, Conditions.class);
+        assertEquals(conditions, deserialized2);
     }
 }
